44c356529bd2c0385b9a9f93808b2dce975e5f95
GiordanoBruno-dev <giordano01rosales@gmail.com>
2025-07-10 16:55:09 -0300

    INICIO DO PROJETO CRIACAO DE NOVOS ARQUIVOS EM COMPONENTES E ESTRUTURACAO DO CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css'
+import { Container } from './components/container';
+import { Heading } from './components/Heading';
 
 export function App() {
-console.log('OII');
 
-return (
-<>
-<Heading attr={123} attr2='String'>
-    Olá mundo!
-    </Heading>
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad corporis suscipit, quidem molestias sequi reiciendis deserunt expedita hic dignissimos modi alias iure omnis. Accusantium nisi deserunt facilis. Minus, sint adipisci.
-    </p>
- </>
-);    
+    return (
+        <>
+            <Container>
+                    <Heading>LOGO</Heading>
+            </Container>        
+
+            <Container>
+                   <Heading>MENU</Heading>
+            </Container>           
+        </>
+    );
 }
-
