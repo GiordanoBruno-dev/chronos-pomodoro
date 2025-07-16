4ddc84eec75dbf8835b790dcfee37b38bd3c0757
GiordanoBruno-dev <giordano01rosales@gmail.com>
2025-07-15 22:52:43 -0300

    Movendo Container para seu Propio react component, organizando componentes em pastas padronizadas e criando o componente de logo com icone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import './styles/theme.css';
-import './styles/global.css'
-import { Container } from './components/container';
+import './styles/global.css';
+
 import { Heading } from './components/Heading';
+import { Container }  from './components/Container';
+import { Logo } from './components/Logo';
+
+
 
 export function App() {
+    
 
-    return (
+    return  (
         <>
             <Container>
-                    <Heading>LOGO</Heading>
-            </Container>        
+                <Logo />
+            </Container>    
 
             <Container>
                    <Heading>MENU</Heading>
             </Container>           
+
         </>
     );
 }
